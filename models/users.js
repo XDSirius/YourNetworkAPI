3efa7563897f29cfbb9e55096393bab8cd5b4e00
Squirrel <yasmin_c@Outlook.com>
2023-09-15 10:24:50 -0500

    Add files via upload
    added code from previous attempt

---
+++ models/users.js
@@ -0,0 +1,36 @@
+const {Schema, model} = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username:{
+            type: String,
+            required: true,
+            unique: true,
+            trimmed: true,
+        },
+        email:{
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
+
+        },
+        thoughts:[
+            {
+              type: Schema.Types.ObjectId,
+              ref: 'Thought',
+            },
+          ],
+        friends:[
+            {
+              type: Schema.Types.ObjectId,
+              ref: 'User',
+            },
+          ],
+    }
+)
+
+userSchema
+ .virtual('friendCount').get(function(){return this.friends.length})
+
+ 
