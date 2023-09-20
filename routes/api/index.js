ac98da7e6bda3c70e79c75cded40f91ef0f9f5f8
Yasmin Castillo <yasmin_c@outlook.com>
2023-09-20 11:00:19 -0500

    fixed thoughts controllers and routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
-//const thoughtsRoutes = require('./thoughtsRoutes');
+const thoughtsRoutes = require('./thoughtsRoutes');
 const userRoutes = require('./userRoutes');
 
-//router.use('/thoughts', thoughtsRoutes);
+router.use('/thoughts', thoughtsRoutes);
 router.use('/users', userRoutes);
 
 module.exports = router;
--- routes/api/thoughtsRoutes.js
+++ routes/api/thoughtsRoutes.js
@@ -1,9 +1,15 @@
 const router = require('express').Router();
 const {
     getThoughts,
-    getSingleThoughts,
-    createThoughts,
+    updateThought,
+    createThought,
+    getSingleThought,
+    deleteThought,
+    createReaction,
+    deleteReaction,
 } = require('../../controllers/thoughtsControllers');
 
-router.route('/').get(getThoughts).post(postThoughts);
-
+router.route('/').get(getThoughts).post(createThought);
+router.route('/:thoughtId').get(getSingleThought).delete(deleteThought).put(updateThought);
+router.route('/:thoughtId/reactions').post(createReaction).delete(deleteReaction);
+module.exports = router;
